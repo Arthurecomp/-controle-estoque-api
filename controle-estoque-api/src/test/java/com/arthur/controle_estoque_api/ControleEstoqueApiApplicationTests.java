d97dce840bd8b1a4c0a66a31e141d2e6868d88ae
Arthurecomp <arthurecomp22@gmail.com>
2026-07-28 12:39:19 -0300

    test: add fake rabbit template for context

--- controle-estoque-api/src/test/java/com/arthur/controle_estoque_api/ControleEstoqueApiApplicationTests.java
+++ controle-estoque-api/src/test/java/com/arthur/controle_estoque_api/ControleEstoqueApiApplicationTests.java
@@ -1,10 +1,9 @@
 package com.arthur.controle_estoque_api;
 
 import com.arthur.controle_estoque_api.config.TestMockConfig;
-import com.arthur.controle_estoque_api.config.TestRabbitConfig;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.amqp.autoconfigure.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
